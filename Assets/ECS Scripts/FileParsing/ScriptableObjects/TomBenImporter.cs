fef41f25c6fed6b03362bb13c2210226c23cdd4e
Haven <woodlandnwater@gmail.com>
2025-02-05 23:13:37 +0000

    break time

--- Assets/ECS Scripts/FileParsing/ScriptableObjects/TomBenImporter.cs
+++ Assets/ECS Scripts/FileParsing/ScriptableObjects/TomBenImporter.cs
@@ -17,6 +17,14 @@ public class TomBenImporter : ScriptedImporter
         cluster,
         wave
     }
+
+    public enum BlockDataState
+    {
+        type,
+        cluster,
+        wave
+    }
+
     public struct ParsedBlock
     {
         public BlockType type;
@@ -27,6 +35,44 @@ public class TomBenImporter : ScriptedImporter
         public override string ToString() =>
             $"ParsedBlock(type={type}, id={id}, name=={name}, content={content})";
     }
+
+    public struct Type
+    {
+        public int id;
+        [CanBeNull] public string typeName;
+        public float? health;
+        public float? speed;
+        public float? damage;
+    }
+
+    public struct Wave
+    {
+        public struct WaveContent
+        {
+            public bool isCluster;
+            public int id;
+            public float? SpawnTime;
+            public float? Pop;
+        }
+        public int id;
+        [CanBeNull] public string waveName;
+        public WaveContent[] waveContent;
+
+        
+    }
+
+    public struct Cluster
+    {
+        public struct ClusterContent
+        {
+            public int id;
+            public int amount;
+        }
+        public int type;
+        public string clusterName;
+        public ClusterContent[] clusterContent;
+    }
+
     enum ParserState
     {
         OutsideBlock,
@@ -45,7 +91,13 @@ public class TomBenImporter : ScriptedImporter
 
     ParserState state;
 
+    BlockDataState blockDataState;
+
     List<ParsedBlock> blocks;
+
+    Dictionary<int, Type> typeDictionary = new Dictionary<int, Type>();
+    Dictionary<int, Wave> waveDictionary = new Dictionary<int, Wave>();
+    Dictionary<int, Cluster> clusterDictionary = new Dictionary<int, Cluster>();
 
     AssetImportContext context;
 
@@ -131,6 +183,80 @@ public class TomBenImporter : ScriptedImporter
                     break;
             }
 
+        }
+        FirstParseComplete();
+    }
+
+    void FirstParseComplete()
+    {
+        charIndex = 0;
+        for (int i = 0; i < blocks.Count; i++)
+        {
+            if (blocks[i].type == BlockType.type) 
+            { blockDataState = BlockDataState.type; }
+            else if (blocks[i].type == BlockType.wave) 
+            { blockDataState = BlockDataState.wave; }
+            else if (blocks[i].type == BlockType.cluster) 
+            { blockDataState = BlockDataState.cluster; }
+            ParseBodyContent(blocks[i].id, blocks[i].name, blocks[i].content);
+
+        }
+    }
+
+    void ParseBodyContent(int id, string name, string content)
+    {
+        charBuffer = content;
+        switch(blockDataState)
+        {
+            case BlockDataState.type:
+                string[] contentBlocks = charBuffer.Split("!?");
+                Regex contentRegex = new Regex(@"(health|speed|damage)=>(\\d+)");
+                float typeHealth = 0;
+                float typeSpeed = 0;
+                float typeDamage = 0;
+
+                for(int i = 0;i<contentBlocks.Length; i++)
+                {
+                    Match contentMatch = contentRegex.Match(contentBlocks[i]);
+                    if (contentMatch.Success)
+                    {
+                        string chunkType = contentMatch.Groups[1].Value;
+                        if(chunkType == "health")
+                        {
+                            if (float.TryParse(contentMatch.Groups[2].Value, out float hOut))
+                            {
+                                typeHealth = hOut;
+                            }
+                            
+                        }
+                        else if (chunkType == "speed")
+                        {
+                            if (float.TryParse(contentMatch.Groups[2].Value, out float sOut))
+                            {
+                                typeSpeed = sOut;
+                            }
+                        }
+                        else if(chunkType == "damage")
+                        {
+                            if (float.TryParse(contentMatch.Groups[2].Value, out float dOut))
+                            {
+                                typeDamage = dOut;
+                            }
+                        }
+                    }
+                    Type type = new Type()
+                    {
+                        typeName = name is "" ? null : name,
+                        id = id,
+                        health = typeHealth is 0 ? null : typeHealth,
+                        speed = typeSpeed is 0 ? null : typeSpeed,
+                        damage = typeDamage is 0 ? null : typeDamage
+
+                    };
+
+                }
+
+                break;
         }
     }
 }
